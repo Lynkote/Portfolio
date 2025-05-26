fa4c5676814ac7faec7331f4127b35901570fa97
Lynkote <lynkoncosta2005@gmail.com>
2025-05-26 15:19:22 -0300

    Pequenas alterações estéticas, remoção da capacidade de selecionar os cartões e o footer e uma reformulação no sistema de geração de cartões de redirecionamento

--- scripts/script.js
+++ scripts/script.js
@@ -1,28 +1,39 @@
+// content
 const PROJECTS = document.getElementById("projects");
-
-// Links
-const DAYLY_QUESTS = "#"
-
+  
+// data 
+const CARDS = [
+  {
+    name: "Daily Quests", 
+    image: 'daily_quests.jpg', 
+    link: "https://lynkote.github.io/daily-quests/"
+  },
+  {
+    name: "Em breve...", 
+    image: 'card.jpg', 
+    link: "#"
+  }
+]
+  
 // Create
-// name, image (160/200px), link
-createCard("Daily Quests", "daily_quests.jpg", DAYLY_QUESTS);
-createCard("Em breve...", "card_img.png", "#");
+createCards();
 
 // Functions
-// create element and add on doom
-function createCard(name, image, url) {
-  let newItem = document.createElement("div");
-  newItem.classList.add("card");
-
-  newItem.innerHTML =
-  `
-    <p>${name}</p>
-    <img src="assets/images/${image}" alt="redirecionamento">
-  `
+// create element and add on DOM
+function createCards() {
+  for (let i = 0; i < CARDS.length; i++) {
+    let newItem = document.createElement("div");
+    newItem.classList.add("card");
   
-  newItem.addEventListener("click", () => {
-    window.open(`${url}`, '_blank');
-  })
-
-  PROJECTS.appendChild(newItem);
+    newItem.innerHTML =
+    `
+      <p>${CARDS[i].name}</p>
+      <img src="assets/images/${CARDS[i].image}" alt="cartão de redirecionamento para: ${CARDS[i].name}">
+    `
+    
+    newItem.addEventListener("click", () => {
+      window.open(`${CARDS[i].link}`, "_blank");
+    })
+    PROJECTS.appendChild(newItem);
+  }
 }
